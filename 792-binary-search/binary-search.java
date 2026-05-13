6d0e767bb5dd0f35da38f92d1586c09a71e5110c
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-05-13 12:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (94.14%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,14 +1,23 @@
 class Solution {
+    public int helper(int[] arr,int target,int low,int high){
+        if(low>high) return -1;
+        int mid = low+(high-low)/2;
+        if(arr[mid] == target) return mid;
+        if(arr[mid]<target) return helper(arr,target,mid+1,high);
+        else return helper(arr,target,low,mid-1);
+    }
     public int search(int[] arr, int target) {
-        int n = arr.length;
-        int low =0 ,  high= n-1;
-        while(low<=high){
-            int mid  = low+(high-low)/2;
-            if(arr[mid] == target) return mid;
-            else if(arr[mid]>target) high = mid-1;
-            else low = mid+1;
-        
-        } 
-        return -1;
+       //by recursion
+       return helper(arr,target,0,arr.length-1);
     }
 }
+//  int n = arr.length;
+//         int low =0 ,  high= n-1;
+//         while(low<=high){
+//             int mid  = low+(high-low)/2;
+//             if(arr[mid] == target) return mid;
+//             else if(arr[mid]>target) high = mid-1;
+//             else low = mid+1;
+        
+//         } 
+//         return -1;
