30f4e6dfe5277b5483b7b779f28f6f7a62291f64
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-05-24 21:19:34 +0530

    Time: 16 ms (22.03%) | Memory: 46.4 MB (32.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,44 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
-            return false;
+        if(s.length() != t.length()) return false;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ele : s.toCharArray()){
+            if(map.containsKey(ele)){
+                int freq = map.get(ele);
+                map.put(ele,freq+1);
+            }
+            else map.put(ele,1);
         }
-        s = s.toLowerCase();
-        t = t.toLowerCase();
-        s = s.replace(" ","");
-        t = t.replace(" ","");
-
-        int count[] = new int[26];
-        for(int i = 0 ; i < s.length() ; i++){
-            count[s.charAt(i) - 'a']++;
-        }
-        for(int i = 0 ; i < t.length() ; i++){
-            count[t.charAt(i) - 'a']--;
-        }
-        for(int i = 0;i<26;i++){
-            if(count[i] != 0) return false;
+        for(char ele : t.toCharArray()){
+            if(map.containsKey(ele)){
+               int freq = map.get(ele);
+               if(freq == 0){
+                 return false;
+               }
+               map.put(ele,freq-1); 
+            }
+            else return false;
         }
         return true;
     }
 }
+
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        // s = s.toLowerCase();
+        // t = t.toLowerCase();
+        // s = s.replace(" ","");
+        // t = t.replace(" ","");
+
+        // int count[] = new int[26];
+        // for(int i = 0 ; i < s.length() ; i++){
+        //     count[s.charAt(i) - 'a']++;
+        // }
+        // for(int i = 0 ; i < t.length() ; i++){
+        //     count[t.charAt(i) - 'a']--;
+        // }
+        // for(int i = 0;i<26;i++){
+        //     if(count[i] != 0) return false;
+        // }
+        // return true;
