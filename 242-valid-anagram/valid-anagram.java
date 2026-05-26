28337a3236ce7b729b5247ce99b348e25678a8da
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-05-26 20:42:12 +0530

    Time: 13 ms (29.77%) | Memory: 46.7 MB (15.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,44 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ele : s.toCharArray()){
-            if(map.containsKey(ele)){
-                int freq = map.get(ele);
-                map.put(ele,freq+1);
-            }
-            else map.put(ele,1);
+       if(s.length() != t.length()) return false;
+       HashMap<Character,Integer> map = new HashMap<>();
+       for(char ch : s.toCharArray()){
+         map.put(ch,map.getOrDefault(ch,0)+1);
+       }
+
+       for(char ch : t.toCharArray()){
+        if(map.containsKey(ch)){
+            int freq = map.get(ch);
+            if(freq == 0) return false;
+            map.put(ch,freq-1);
         }
-        for(char ele : t.toCharArray()){
-            if(map.containsKey(ele)){
-               int freq = map.get(ele);
-               if(freq == 0){
-                 return false;
-               }
-               map.put(ele,freq-1); 
-            }
-            else return false;
-        }
-        return true;
+        else return false;
+       }
+       return true;
     }
 }
 
-        // if (s.length() != t.length()) {
-        //     return false;
-        // }
-        // s = s.toLowerCase();
-        // t = t.toLowerCase();
-        // s = s.replace(" ","");
-        // t = t.replace(" ","");
-
-        // int count[] = new int[26];
-        // for(int i = 0 ; i < s.length() ; i++){
-        //     count[s.charAt(i) - 'a']++;
-        // }
-        // for(int i = 0 ; i < t.length() ; i++){
-        //     count[t.charAt(i) - 'a']--;
-        // }
-        // for(int i = 0;i<26;i++){
-        //     if(count[i] != 0) return false;
-        // }
-        // return true;
+       
