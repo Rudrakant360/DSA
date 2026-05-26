e460b39069e9e1c755866e0d120122a516c630d1
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-05-26 20:24:28 +0530

    Time: 37 ms (5.19%) | Memory: 97 MB (14.68%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele : nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        for(int ele : map.keySet()){
+            if(map.get(ele)>1) return true;
+        }
+        return false;
+    }
+}
