6a47f88472e6e545b47118873299e329214094c2
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-05-27 20:12:14 +0530

    Time: 122 ms (7.20%) | Memory: 47.6 MB (9.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,20 @@
 class Solution {
-    public boolean helper(int left,int right,String s){
-         if(left>=right) return true;
-         if(s.charAt(left) != s.charAt(right)) return false;
-         return helper(left+1,right-1,s);
+    public boolean helper(String s){
+        for(int i = 0;i<s.length()/2;i++){
+            if(s.charAt(i) != s.charAt(s.length()-1-i)) return false;
+        }
+        return true;
     }
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        String ans = "";
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if((ch >= 'a' && ch <= 'z') ||(ch >= '0' && ch <= '9')){
-                ans+=ch;
-            }
+    s = s.toLowerCase();
+    String str = "";
+    for(int i = 0 ; i<s.length();i++){
+        char ch = s.charAt(i);
+        if((ch>='a' && ch<='z')||(ch>= '0' && ch<= '9')){
+           str+=ch;
         }
-       return helper(0,ans.length()-1,ans);
+    }
+    return helper(str);
+       
     }
 }
