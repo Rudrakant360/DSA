3fc065464c2883f04f1c3c722735c0549d09a757
Rudrakant360 <rudrakantsahoo@gmail.com>
2026-06-06 11:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (77.79%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,42 +1,67 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2; 
+     Queue<Integer> q;
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q = new LinkedList<>(); 
     }
     
     public void push(int x) {
-        q1.add(x); 
+      q.add(x);
+      for(int i = 0;i<q.size()-1;i++){
+        q.add(q.remove());
+      }   
     }
     
     public int pop() {
-       while(q1.size()>1){
-         q2.add(q1.remove());
-       }
-       int x = q1.remove();
-       Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
-       return x; 
+        return q.remove();
     }
     
     public int top() {
-        int x = -1;
-       while(q1.size()>0){
-        x = q1.remove();
-         q2.add(x);
-       }
-       Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
-       return x; 
+        return q.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+       return q.isEmpty(); 
     }
 }
+// class MyStack {
+//     Queue<Integer> q1;
+//     Queue<Integer> q2; 
+//     public MyStack() {
+//         q1 = new LinkedList<>();
+//         q2 = new LinkedList<>();
+//     }
+    
+//     public void push(int x) {
+//         q1.add(x); 
+//     }
+    
+//     public int pop() {
+//        while(q1.size()>1){
+//          q2.add(q1.remove());
+//        }
+//        int x = q1.remove();
+//        Queue<Integer> temp = q1;
+//         q1 = q2;
+//         q2 = temp;
+//        return x; 
+//     }
+    
+//     public int top() {
+//         int x = -1;
+//        while(q1.size()>0){
+//         x = q1.remove();
+//          q2.add(x);
+//        }
+//        Queue<Integer> temp = q1;
+//         q1 = q2;
+//         q2 = temp;
+//        return x; 
+//     }
+    
+//     public boolean empty() {
+//         return q1.isEmpty();
+//     }
+// }
 
 /**
  * Your MyStack object will be instantiated and called as such:
